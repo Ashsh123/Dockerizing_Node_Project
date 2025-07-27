f48fe1794443001685a60d786d14ccd96f98b495
Ashish Rathor <rathorashish654@gmail.com>
2025-07-27 22:59:02 +0530

    Added codes

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
+const dotenv = require('dotenv')
+dotenv.config();
+
 const app = express();
 
 app.get('/', (req,res)=>{
     res.json({mesage:"Hello server"})
 })
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT ,()=>{
     console.log('server created successfully...');
 })
